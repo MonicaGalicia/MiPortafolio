e4e195566db7326b7cce918d9a324fa69824ad56
MonicaGalicia <monica.garmz@gmail.com>
2016-11-22 19:08:23 +0000

    cambios

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 var i=0;
-var texto= ' "Make things happen." ' ;
+var texto= 'Desarrolladora Web Front End .Apasionada del desarrollo Web . Amante de la naturaleza.Perseverante.Soñadora, Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus sed nisi dicta. Tempore cupiditate, dolorem nostrum eos illo rerum. Illum modi facere aliquam architecto veniam commodi, fuga vel itaque consectetur! ' ;
 
 $(document).ready(function(){
 	function pintarLetra(){
 		$("#texto").append(texto[i]);
 		if(i<texto.length){
 			i++;
-			setTimeout(pintarLetra,200);
+			setTimeout(pintarLetra,100);
 		}
 	};
 	pintarLetra();
@@ -24,6 +24,10 @@ $( document ).ready(function(){
       $(".texto-animado").removeClass("hidden");
 	})
 },500)
+
+
+
+
 
 
 
